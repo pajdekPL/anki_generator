6626e2b8236afc862fd9a3174094937c0a5bb254
Adam Pajda <adam_pajda@outlook.com>
2022-12-12 20:10:12 +0100

    INIT commit, working anki_generator

---
+++ anki_generator_app/anki_generator.py
@@ -0,0 +1,53 @@
+import tempfile
+from pathlib import Path
+from anki_generator_app.downloader import get_webpage_content, download_file
+from anki_generator_app.genanki_handler import GenankiVocabHandler
+from anki_generator_app.dictionaries import get_dictionary_handler
+
+
+class AnkiGenerator:
+    """
+    Anki generator, example of usage:
+
+    anki_output_file_path = Path("tests/data/my_deck.apkg")
+    anki_generator = AnkiGenerator("TestDeck")
+    anki_generator.add_word("prudent")
+    anki_generator.add_word("deride")
+    anki_generator.generate_flashcards(anki_output_file_path)
+    """
+
+    def __init__(self, deck_name: str, dictionary: str = "Cambridge"):
+        self.dictionary = get_dictionary_handler(dictionary)
+        self.anki_handler = GenankiVocabHandler(deck_name)
+        self.words = []
+
+    def add_word(self, word: str):
+        self.words.append(word)
+
+    def add_words_from_txt_file(self, path_to_file: Path):
+        with open(path_to_file) as file:
+            for line in file.readlines():
+                self.add_word(line.lower().strip())
+
+    def generate_flashcards(self, output_file: Path):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            for word in self.words:
+                word_webpage = get_webpage_content(self.dictionary.get_word_url(word))
+                meanings = self.dictionary.get_word_meanings(word_webpage)
+                ipa = self.dictionary.get_ipa(word_webpage)
+                examples_of_usage = self.dictionary.get_word_examples_of_usage(word_webpage)
+                media_file_name = f"{word}.mp3"
+                mp3_media_file_path = Path(temp_dir)/media_file_name
+                mp3_media_url = self.dictionary.get_word_us_pronunciation_url(word_webpage)
+                download_file(mp3_media_url, mp3_media_file_path)
+                self.anki_handler.add_vocab_flashcard_to_deck(mp3_media_file_path, word, meanings,
+                                                              examples_of_usage, ipa, media_file_name)
+            self.anki_handler.save_apkg_anki_file(output_file)
+
+
+if __name__ == "__main__":
+    words = ["person", "use"]
+    anki_output_file_path = Path("tests/data/prudent.apkg")
+    anki_generator = AnkiGenerator("TestDeck")
+    anki_generator.add_word("prudent")
+    anki_generator.generate_flashcards(anki_output_file_path)
---
+++ anki_generator_app/tests/test_integration.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+from unittest.mock import patch, call
+from pytest import fixture
+from anki_generator_app.anki_generator import AnkiGenerator
+import tempfile
+
+
+def test_anki_flashcard_is_properly_generated_for_word_prudent(mocked_requests):
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+               'Chrome/51.0.2704.103 Safari/537.36'}
+    deck_name = "TestDeck"
+    expected_apkg_file_size = 57
+    word = "prudent"
+    requests_get_expected_calls = [
+        call('https://dictionary.cambridge.org/dictionary/english/prudent', headers=headers),
+        call('https://dictionary.cambridge.org//media/english/us_pron/p/pru/prude/prudent.mp3',headers=headers)
+    ]
+
+    class WordWebPageContent:
+        web_page_for_word = Path("data/prudent.html")
+        with open(web_page_for_word) as file:
+            content = file.read()
+
+    class PrudentMp3:
+        prudent_mp3_file = Path("data/prudent.mp3")
+        with open(prudent_mp3_file, 'rb') as file:
+            content = file.read()
+
+    mocked_requests.get.side_effect = [WordWebPageContent, PrudentMp3]
+
+    with tempfile.NamedTemporaryFile() as file:
+        output_file = Path(file.name)
+        anki_generator = AnkiGenerator(deck_name)
+        anki_generator.add_word(word)
+        anki_generator.generate_flashcards(output_file)
+        assert int(os.stat(output_file).st_size / 1024) == expected_apkg_file_size
+    mocked_requests.get.assert_has_calls(requests_get_expected_calls, any_order=False)
+
+
+@fixture()
+def mocked_requests():
+    with patch("anki_generator_app.downloader.requests") as mock_requests:
+        yield mock_requests
